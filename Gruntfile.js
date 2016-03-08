c004ea83b732c760a1a0ad1e98535a34b1112bad
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 23:54:07 +0530

    Initial commit
    The Heart of Build System of "Vitae®".

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,23 @@
 #!/usr/bin/env node
+
+
+/*!
+ * Vitae® [v1.0.0]
+ * ______________________________________________________________
+ * Grunt, http://gruntjs.com/
+ * The JavaScript Task Runner.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ______________________________________________________________
+ * @date      : 08-March-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "Vitae®".
+ * ______________________________________________________________
+ */
