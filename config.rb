bf23c1ce8072bc67016c7bbf43b6f372e3b0fdbc
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 23:41:01 +0530

    Initial commit
    AXIX® - compass based framework.

--- config.rb
+++ config.rb
@@ -1,1 +1,19 @@
 #!/usr/bin/env ruby
+
+
+# AXIX®
+# ---------------------------------------
+# From the Desk of Prabhat Kumar.
+# @author     : Prabhat Kumar.
+# @copyright  : Prabhat Kumar.
+# @homepage   : http://prabhatkumar.org/
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.0
+# @build      : Everest
+# @date       : 01-Jan-2016
+# @license    : Apache, version 2.0
+# ---------------------------------------
+# Principle  : Standard Module Definition
+# ---------------------------------------
