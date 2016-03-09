cc9942fd92092b91feb7d236e1c8ed0c911d8bcf
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-09 09:21:32 +0530

    Initial commit
    The Non-AMD JavaScript - for defining "Curriculum Vitaé" data.

---
+++ source/core/data/vitae.js
@@ -0,0 +1,18 @@
+/*!
+ * Vitaé® [v1.0.0]
+ * _______________________________________________________________
+ * AXIX®, https://github.com/AXIX
+ * The Non-AMD JavaScript - for defining "Curriculum Vitaé" data.
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _______________________________________________________________
+ * @date      : 09-March-2016
+ * @license   : Apache, version 2.0
+ * @require   : classie
+ * @require   : Node.js®
+ * @require   : NPM
+ * _______________________________________________________________
+ */
